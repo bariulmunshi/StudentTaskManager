59cdf33a2400b6dbfa99270b1bfe4fb3f5e03bf9
Md Bariul Munshi <bariulsoft@gmail.com>
2026-06-23 18:18:35 +0600

    Step 8: Added HomeFragment inside DashboardActivity

--- app/src/main/java/com/research/studenttaskmanager/DashboardActivity.kt
+++ app/src/main/java/com/research/studenttaskmanager/DashboardActivity.kt
@@ -1,7 +1,6 @@
 package com.research.studenttaskmanager
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class DashboardActivity : AppCompatActivity() {
@@ -10,10 +9,8 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        val userName = intent.getStringExtra("USER_NAME")
-
-        val tvWelcome = findViewById<TextView>(R.id.tvWelcome)
-
-        tvWelcome.text = "Welcome $userName"
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, HomeFragment())
+            .commit()
     }
 }
